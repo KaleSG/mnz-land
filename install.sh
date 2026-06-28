b9808dceef32a7da06efb5366b7b1b0dbdbe69bb
KaleSG <jeremiahsuarez06@gmail.com>
2026-06-27 22:56:12 -0700

    idk

--- install.sh
+++ install.sh
@@ -4,6 +4,16 @@ sudo wget -O /usr/local/share/keyrings/mnz-land-keyring.key https://kalesg.githu
 sudo wget -O /etc/apt/sources.list.d/mnz-land.sources https://kalesg.github.io/mnz-land/mnz-land.sources
 
 sudo apt update
-sudo apt install mnz-land
+sudo apt install mnz-land -y
+ 
+mkdir -p ~/.config
 
+git clone  --filter=blob:none --no-checkout https://github.com/KaleSG/mnz-land.git
+cd mnz-land
+git init sparse-checkout init --cone
+git space-checkout set .config
 
+git checkout
+
+cp .config/* ~/.config/
+rm -rf mnz-land
