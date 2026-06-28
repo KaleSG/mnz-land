8811cdf27da1db566248f9234d95517967c41382
KaleSG <jeremiahsuarez06@gmail.com>
2026-06-27 18:46:05 -0700

    fixed install

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo wget -O /usr/local/share/keyrings/mnz-land-keyring.key https://kalesg.github.io/mnz-land/mnz-land-keyring.key
-sudo wget -O /etc/apt/sources.list.d/mnz-land.sources /etc/apt/sources.list.d/mnz-land.sources
+sudo wget -O /etc/apt/sources.list.d/mnz-land.sources https://kalesg.github.io/mnz-land/mnz-land.sources
 
 sudo apt update
 sudo apt install mnz-land
