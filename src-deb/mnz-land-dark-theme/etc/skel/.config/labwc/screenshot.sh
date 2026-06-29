81097002f54a238a9ee8565e27869c6b480c6635
KaleSG <jeremiahsuarez06@gmail.com>
2026-06-28 21:24:57 -0700

    updated reprepro exports

--- src-deb/mnz-land-dark-theme/etc/skel/.config/labwc/screenshot.sh
+++ src-deb/mnz-land-dark-theme/etc/skel/.config/labwc/screenshot.sh
@@ -9,5 +9,5 @@ FILE=~/screenshots/$(date +%Y-%m-%d_%H-%M-%S).png
 if grim -g "$(slurp)" "$FILE"; then
     # Send notification. Clicking it opens the screenshot folder via xdg-open.
     ACTION=$(notify-send "Screenshot Saved" "Captured to $FILE" -i $FILE)
-
+    wl-copy < $FILE
 fi
